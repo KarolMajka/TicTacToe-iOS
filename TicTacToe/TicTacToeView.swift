1a5b3a8abd4d7cbff7ff317d37d5782ead2150a3
Karol Majka <karol.x.majka@gsk.com>
2017-05-04 15:21:50 +0200

    add TicTacToeView to project

---
+++ TicTacToe/TicTacToeView.swift
@@ -0,0 +1,22 @@
+//
+//  TicTacToeView.swift
+//  TicTacToe
+//
+//  Created by Karol Majka on 04/05/2017.
+//  Copyright © 2017 karolmajka. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TicTacToeView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
