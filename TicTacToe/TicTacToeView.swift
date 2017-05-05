c3dbfa9463844550ff5e8f001b27f3a4d1fcd0ae
Karol Majka <karol.x.majka@gsk.com>
2017-05-05 11:57:20 +0200

    change from programmatically create TicTacToe board to .xib

--- TicTacToe/TicTacToeView.swift
+++ TicTacToe/TicTacToeView.swift
@@ -10,71 +10,62 @@ import Foundation
 import UIKit
 
 class TicTacToeView: UIView {
+    
+    var mainView: UIView!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.createSubviews()
+        self.createView()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        self.createView()
     }
     
-    func createSubviews() {
-        self.translatesAutoresizingMaskIntoConstraints = false
-        let size = min(self.frame.size.width, self.frame.size.height)/3
-        for i in 0...8 {
-            let view = UIView(frame: CGRect(x: 0, y: 0, width: size, height: size))
-            view.translatesAutoresizingMaskIntoConstraints = false
-            view.tag = 100+i
-            view.backgroundColor = UIColor.clear
-            
-            //Height and Width Constraint
-            if i == 0 {
-                let widthConstraint = NSLayoutConstraint.init(item: view, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.width, multiplier: 1.0, constant: size)
-                let heightConstraint = NSLayoutConstraint.init(item: view, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.height, multiplier: 1.0, constant: size)
-                view.addConstraints([widthConstraint, heightConstraint])
-            } else {
-                let view2 = self.viewWithTag(100)!
-                let widthConstraint = NSLayoutConstraint.init(item: view, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: view2, attribute: NSLayoutAttribute.width, multiplier: 1.0, constant: 0)
-                let heightConstraint = NSLayoutConstraint.init(item: view, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: view2, attribute: NSLayoutAttribute.height, multiplier: 1.0, constant: 0)
-                view.addConstraints([widthConstraint, heightConstraint])
-            }
-            
-            //Top Constraint
-            if i < 3 {
-                let constraint = NSLayoutConstraint.init(item: self, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.top, multiplier: 1.0, constant: 0)
-                self.addConstraint(constraint)
-            } else {
-                let view2 = self.viewWithTag(100+i-3)!
-                let constraint = NSLayoutConstraint.init(item: view2, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.top, multiplier: 1.0, constant: 0)
-                view.addConstraint(constraint)
-            }
-            
-            //Leading Constraint
-            if i % 3 == 0 {
-                let constraint = NSLayoutConstraint.init(item: self, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.leading, multiplier: 1.0, constant: 0)
-                self.addConstraint(constraint)
-            } else {
-                let view2 = self.viewWithTag(100+i-3)!
-                let constraint = NSLayoutConstraint.init(item: view2, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.leading, multiplier: 1.0, constant: 0)
-                view.addConstraint(constraint)
-            }
-            
-            //Trailing Constraint
-            if i % 3 == 2 {
-                let constraint = NSLayoutConstraint.init(item: self, attribute: NSLayoutAttribute.trailing, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.trailing, multiplier: 1.0, constant: 0)
-                self.addConstraint(constraint)
-            }
-            
-            //Bottom Constraint
-            if i > 5 {
-                let constraint = NSLayoutConstraint.init(item: self, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.bottom, multiplier: 1.0, constant: 0)
-                self.addConstraint(constraint)
-            }
-            
-            self.addSubview(view)
-        }
+    func createView() {
+        self.addView()
+        self.addLines()
+    }
+    
+    func addView() {
+        let view = (Bundle.main.loadNibNamed("TicTacToeView", owner: self, options: nil)![0] as! UIView).subviews[0]
+        view.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(view)
+        self.mainView = view
         
+        let centerX = NSLayoutConstraint.init(item: view, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.centerX, multiplier: 1.0, constant: 0)
+        let centerY = NSLayoutConstraint.init(item: view, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.centerY, multiplier: 1.0, constant: 0)
+        let widthConstraint = NSLayoutConstraint.init(item: view, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.width, multiplier: 1.0, constant: 0)
+        let heightConstraint = NSLayoutConstraint.init(item: view, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.height, multiplier: 1.0, constant: 0)
+        self.addConstraints([centerX, centerY, widthConstraint, heightConstraint])
+    }
+    
+    func addLines() {
+        self.addLine(from: CGPoint(x: self.mainView.frame.size.width/3, y: 0), to: CGPoint(x: self.mainView.frame.size.width/3, y: self.mainView.frame.size.height))
+        self.addLine(from: CGPoint(x: self.mainView.frame.size.width/3*2, y: 0), to: CGPoint(x: self.mainView.frame.size.width/3*2, y: self.mainView.frame.size.height))
+        
+        self.addLine(from: CGPoint(x: 0, y: self.mainView.frame.size.height/3), to: CGPoint(x: self.mainView.frame.size.width, y: self.mainView.frame.size.height/3))
+        self.addLine(from: CGPoint(x: 0, y: self.mainView.frame.size.height/3*2), to: CGPoint(x: self.mainView.frame.size.width, y: self.mainView.frame.size.height/3*2))
+        
+        self.setNeedsDisplay()
+    }
+    
+    func addLine(from: CGPoint, to: CGPoint) {
+        let path = UIBezierPath()
+        
+        path.move(to: from)
+        path.addLine(to: to)
+        path.close()
+        
+        let layer = CAShapeLayer()
+        
+        layer.path = path.cgPath
+        layer.lineWidth = 4
+        layer.strokeColor = UIColor.blue.cgColor
+        layer.fillColor = UIColor.clear.cgColor
+        
+        self.mainView.layer.addSublayer(layer)
     }
     
 }
